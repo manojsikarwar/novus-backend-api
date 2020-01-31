2be47cbbedc3fb52812caa7e9f9ad52e5b45c5dd
kapilkarda <kkarda77@gmail.com>
2020-01-31 18:56:58 +0530

    new

---
+++ db.js
@@ -0,0 +1,37 @@
+const {Client} 	= require('pg');
+
+
+// const client = new Client({
+//     user	:'postgres',
+// 	password:'postgres',
+// 	host	:'localhost',
+// 	port 	: 5432,
+// 	database:'redis',
+// 	ssl		: false
+// })
+const client = new Client({
+    user	:'postgres',
+	password:'postgres',
+	host	:'localhost',
+	port 	: 5432,
+	database:'redis',
+	ssl		: false
+})
+
+
+// if(client._connected == false){
+
+// 		console.log('Connection not found');
+// }else{
+	client.connect().then((conn)=>{
+		console.log('Connection Successfully');
+
+	})
+// }
+
+
+
+
+module.exports=client;
+// console.log(client._connected)
+
