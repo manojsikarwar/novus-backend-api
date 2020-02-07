b1cbc3ccea4605d0d9e081f73e542fbd8410fe10
kapilkarda <kkarda77@gmail.com>
2020-02-07 19:53:19 +0530

    issue fixed

--- db.js
+++ db.js
@@ -11,8 +11,8 @@ const {Client} 	= require('pg');
 // })
 const client = new Client({
     user	:'postgres',
-	password:'admin',
-	host	:'postgres',
+	password:'postgres',
+	host	:'localhost',
 	port 	: 5432,
 	database:'redis',
 	ssl		: false
