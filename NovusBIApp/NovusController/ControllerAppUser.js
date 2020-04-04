3d4a83390bfb7c4989b722c3314ce43ba68528e9
manojsikarwar <manojsikarwar22@gmail.com>
2020-04-04 17:13:48 +0530

    changes

--- NovusBIApp/NovusController/ControllerAppUser.js
+++ NovusBIApp/NovusController/ControllerAppUser.js
@@ -121,8 +121,8 @@ module.exports.appuser_login = (body) => {
 			const ApplicationId = body.ApplicationId;
 			// resolve(body)
 			if(username != ''){
-				const getuser = `select * from novus_app_user where username = '${username}' `
-				// const getuser = `select * from app_user where user_name = '${username}' and status = '${0}' and application_id = '${ApplicationId}' `
+				// const getuser = `select * from novus_app_user where username = '${username}' `
+				const getuser = `select * from app_user where user_name = '${username}' and status = '${0}' and application_id = '${ApplicationId}' `
 				// console.log(getuser)
 				client.query(getuser, (getusererr, getuserress) => {
 					if(getusererr){
